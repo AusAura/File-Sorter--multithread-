168f3da58917f85bfa9c18915050e9ca482144d1
AusAura <91804331+AusAura@users.noreply.github.com>
2023-09-27 00:56:46 +0400

    Updated with numbers_to_check = range(10_651_060)

--- factorize.py
+++ factorize.py
@@ -7,7 +7,7 @@ stream_handler = logging.StreamHandler()
 logger.addHandler(stream_handler)
 logger.setLevel(logging.DEBUG)
 
-numbers = range(10_00)
+numbers_to_check = range(10_651_060)
 
 def factorize_sync(numbers: list) -> list:
     timer = time()
@@ -16,7 +16,7 @@ def factorize_sync(numbers: list) -> list:
         if number1 == 0:
             continue
         perfect_numbers[number1] = []
-        for number2 in numbers:
+        for number2 in numbers_to_check:
             if number2 == 0:
                 continue
             if number1 % number2 == 0:
@@ -34,7 +34,7 @@ def factorize_multipr(number: int) -> list:
         return perfect_numbers
     
     perfect_numbers[number] = []
-    for number2 in numbers:
+    for number2 in numbers_to_check:
         if number2 == 0:
                 continue
         if number % number2 == 0:
@@ -43,13 +43,13 @@ def factorize_multipr(number: int) -> list:
     return perfect_numbers
 
 if __name__ == '__main__':
-    result = factorize_sync(numbers)
+    result = factorize_sync(numbers_to_check)
     print("SYNC NUMBERS: ", result[0])
     sync_res = f"SYNC RESULT: {result[1]}"
 
     with Pool(processes=cpu_count()) as pool:
         timer = time()
-        returned_dict = pool.map(factorize_multipr, numbers)
+        returned_dict = pool.map(factorize_multipr, numbers_to_check)
         for dict in returned_dict:
             perfect_numbers.update(dict)
         print('ASYNC NUMBERS: ', perfect_numbers)
